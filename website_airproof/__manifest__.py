1d4884abbc0c9a06ed1e0febf52bc8eeebe1ed23
Baudouin Ninitte <bani@odoo.com>
2025-10-02 15:40:37 +0200

    [ADD] Add missing items

--- website_airproof/__manifest__.py
+++ website_airproof/__manifest__.py
@@ -34,7 +34,7 @@
             "website_airproof/static/src/scss/layout/header.scss",
             # SNIPPETS
             'website_airproof/static/src/scss/snippets/caroussel.scss',
-            'website_airproof/static/src/scss/snippets/caroussel.scss',
+            'website_airproof/static/src/scss/snippets/newsletter.scss',
             # COMPONENTS
             'website_airproof/static/src/js/mouse_follower.js',
             'website_airproof/static/src/scss/components/mouse_follower.scss',
